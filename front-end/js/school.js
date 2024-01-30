30abf5046311430a0dc037fb5bc366f2e9ec63d2
JhuniorCq <114239158+JhuniorCq@users.noreply.github.com>
2024-01-30 04:33:55 -0500

    Agregando nombres de escuelas

--- front-end/js/school.js
+++ front-end/js/school.js
@@ -2,26 +2,48 @@
 const escuelaSelect = document.getElementById('escuela-select');// El AXIOS da, pero con la CDN, con el import no da :,v
 
 console.log('Se lee este archivo :,v');
+
+
+const response = await axios.get('http://localhost:3000/assignProgram');
+const schoolsData = response.data;
+
+console.log(schoolsData);
+
+const nombresEscuelas = schoolsData.map(datosEscuela => {
+    return datosEscuela[1];
+});
+
+console.log(nombresEscuelas);
+
+const cantidadEscuelas = nombresEscuelas.length;
+
+for(const escuela of nombresEscuelas) {
+    const nuevoOption = document.createElement('option');
+    nuevoOption.innerText = `${escuela}`;
+    escuelaSelect.append(nuevoOption);
+}
+
+
+
+
 const funcion = async () => {
     try {
-        const response = await axios.get('http://localhost:3000/assignProgram');
-        const schoolsData = response.data;
+        // const response = await axios.get('http://localhost:3000/assignProgram');
+        // const schoolsData = response.data;
 
-        console.log(schoolsData);
+        // console.log(schoolsData);
 
-        const nombresEscuelas = schoolsData.map(datosEscuela => {
-            return datosEscuela[1];
-        });
+        // const nombresEscuelas = schoolsData.map(datosEscuela => {
+        //     return datosEscuela[1];
+        // });
 
-        console.log(nombresEscuelas);
+        // console.log(nombresEscuelas);
 
-        const cantidadEscuelas = nombresEscuelas.length;
+        // const cantidadEscuelas = nombresEscuelas.length;
 
-        for(const escuela of nombresEscuelas) {
-            escuelaSelect.innerHTML = `
-                <option>${escuela}</option>
-            `;
-        }
+        // for(const escuela of nombresEscuelas) {
+            
+        // }
 
     } catch(err) {
         console.error('Error en school.js', err.message);
