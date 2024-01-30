6615cf55eae6020972a296953e24607c8c3ddb95
JhuniorCq <114239158+JhuniorCq@users.noreply.github.com>
2024-01-30 04:03:22 -0500

    CDN Axios

--- front-end/js/school.js
+++ front-end/js/school.js
@@ -1,18 +1,11 @@
 
-const escuelaSelect = document.getElementById('escuela-select');// NO DA CON EL AXIOS :,v
+const escuelaSelect = document.getElementById('escuela-select');// El AXIOS da, pero con la CDN, con el import no da :,v
 
 console.log('Se lee este archivo :,v');
 const funcion = async () => {
     try {
         const response = await axios.get('http://localhost:3000/assignProgram');
-        // const response = await fetch('http://localhost:3000/assignProgram');
         const schoolsData = response.data;
-        
-        // if (!response.ok) {
-        //     throw new Error(`Error al realizar la solicitud: ${response.statusText}`);
-        // }
-        
-        // const schoolsData = await response.json();
 
         console.log(schoolsData);
 
