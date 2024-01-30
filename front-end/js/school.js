dc98e2d26d25da3e9af681cf577b53364cb043f4
JhuniorCq <114239158+JhuniorCq@users.noreply.github.com>
2024-01-30 03:16:45 -0500

    usando fetch

--- front-end/js/school.js
+++ front-end/js/school.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+// import axios from 'axios';
 
 const escuelaSelect = document.getElementById('escuela-select');
 
@@ -6,8 +6,14 @@ console.log('Se lee este archivo :,v');
 const funcion = async () => {
     try {
         // const response = await axios.get('https://edunex-solutions.onrender.com/assignProgram');
-        const response = await axios.get('http://localhost:3000/assignProgram');
-        const schoolsData = response.data;
+        // const response = await axios.get('http://localhost:3000/assignProgram');
+        const response = await fetch('http://localhost:3000/assignProgram');
+        
+        if (!response.ok) {
+            throw new Error(`Error al realizar la solicitud: ${response.statusText}`);
+        }
+        
+        const schoolsData = response.json();
 
         console.log(schoolsData);
 
