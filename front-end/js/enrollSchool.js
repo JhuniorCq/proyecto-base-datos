39e616fe4998b326946136fb70b370836f915a1c
JhuniorCq <114239158+JhuniorCq@users.noreply.github.com>
2024-02-05 17:08:13 -0500

    .

--- front-end/js/enrollSchool.js
+++ front-end/js/enrollSchool.js
@@ -1,23 +1,20 @@
+const departamentoSelect = document.getElementById('departamentoSelect');
+const provinciaSelect = document.getElementById('provinciaSelect');
+const distritoSelect = document.getElementById('distritoSelect');
 
+const axiosGet = async (url) => {
+    try {
+        const response = await axios.get(url);
+        return response.data;
+    } catch(err) {
+        console.error('', err.message);
+    }
+}
 
-const response1 = await axios.get('https://geo-peru-api.onrender.com/department');
-const listaDepartamentos = response1.data;
-
-const response2 = await axios.get('https://geo-peru-api.onrender.com/province')
-const listaProvincias = response2.data;
-
-const response3 = await axios.get('https://geo-peru-api.onrender.com/district');
-const listaDistritos = response3.data;
+const listaDepartamentos = await axiosGet('https://geo-peru-api.onrender.com/department');
+const listaProvincias = await axiosGet('https://geo-peru-api.onrender.com/province');
+const listaDistritos = await axiosGet('https://geo-peru-api.onrender.com/district');
 
 console.log(listaDepartamentos);
 console.log(listaProvincias);
 console.log(listaDistritos);
-
-
-// const consumirGeoPeruAPI = async () => {
-//     try {
-//         const response = await axios.get('');
-//     } catch (err) {
-//         console.error('', err.message)
-//     }
-// }
