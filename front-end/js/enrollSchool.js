6afb516ffc44bf524b01ba298457c28ec75275e3
JhuniorCq <114239158+JhuniorCq@users.noreply.github.com>
2024-02-05 16:46:08 -0500

    Consumiendo API

--- front-end/js/enrollSchool.js
+++ front-end/js/enrollSchool.js
@@ -1,5 +1,19 @@
 
 
+const response1 = await axios.get('https://geo-peru-api.onrender.com/department');
+const listaDepartamentos = response1.data;
+
+const response2 = await axios.get('https://geo-peru-api.onrender.com/province')
+const listaProvincias = response2.data;
+
+const response3 = await axios.get('https://geo-peru-api.onrender.com/district');
+const listaDistritos = response3.data;
+
+console.log(listaDepartamentos);
+console.log(listaProvincias);
+console.log(listaDistritos);
+
+
 // const consumirGeoPeruAPI = async () => {
 //     try {
 //         const response = await axios.get('');
