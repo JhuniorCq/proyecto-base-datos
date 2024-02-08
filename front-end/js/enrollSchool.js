ebfde6e1d4712647cb7f84966623d9848a1df59f
JhuniorCq <114239158+JhuniorCq@users.noreply.github.com>
2024-02-08 11:13:06 -0500

    formulario escuelas

--- front-end/js/enrollSchool.js
+++ front-end/js/enrollSchool.js
@@ -1,112 +1,31 @@
+const formulario = document.querySelector('.formulario');
+const codigoModular = document.getElementById('codigo-modula');
+const nombreEscuela = document.getElementById('nombre-escuela');
+const direccionEscuela = document.getElementById('direccion');
 const departamentoSelect = document.getElementById('departamentoSelect');
 const provinciaSelect = document.getElementById('provinciaSelect');
 const distritoSelect = document.getElementById('distritoSelect');
-// const todosSelect = document.querySelectorAll('.select');
 
-// console.log(todosSelect);
+const nombresDirector = document.getElementById('nombres-director');
+const apellidosDirector = document.getElementById('apellidos-director');
+const telefonoDirector = document.getElementById('telefono');
+const emailDirector = document.getElementById('director');
 
-const axiosGet = async (url) => {
-    try {
-        const response = await axios.get(url);
-        return response.data;
-    } catch(err) {
-        console.error('', err.message);
-    }
+const funcion = (evento) => {
+    //Evitar que el formulario se envíe de forma predeterminada, y que se recargue la página
+    evento.preventDefault();
+
+    console.log(codigoModular);
+    console.log(nombreEscuela);
+    console.log(direccionEscuela);
+    console.log(departamentoSelect);
+    console.log(provinciaSelect);
+    console.log(distritoSelect);
+    console.log(nombresDirector);
+    console.log(apellidosDirector);
+    console.log(telefonoDirector);
+    console.log(emailDirector);
 }
 
-//Función sin Evento -> Mostrar Departamentos
-const mostrarDepartamentos = () => {
-    for(const objeto of listaDepartamentos) {
-        const nombreDepartamento = objeto["departamento"];
-        const idDepartamento = objeto["id_departamento"]
-        crearOptionSelect(nombreDepartamento, idDepartamento, departamentoSelect);
-    }
-}
 
-//Función con Evento 'change' -> Mostrar Provincias
-const mostrarProvincias = () => {
-    provinciaSelect.innerHTML = '';
-
-    const contenidoOptionDefault = '-- Escoge una Provincia --';
-    crearOptionDefault(contenidoOptionDefault, provinciaSelect);
-
-    const idDepartamentoSeleccionado = departamentoSelect.value;
-    console.log(idDepartamentoSeleccionado);
-
-    const provinciasSeleccionadas = listaProvincias.filter(provincia => provincia["id_departamento"] === idDepartamentoSeleccionado);
-    console.log(provinciasSeleccionadas);
-    
-    for(const objeto of provinciasSeleccionadas) {
-        const nombreProvincia = objeto["provincia"];
-        const idProvincia = objeto["id_provincia"];
-        crearOptionSelect(nombreProvincia, idProvincia, provinciaSelect);
-    }
-}
-
-//Función con Evento 'change' -> Mostrar Distritos
-const mostrarDistritos = () => {
-    distritoSelect.innerHTML = '';
-
-    const contenidoOptionDefault = '-- Escoge un Distrito --';
-    crearOptionDefault(contenidoOptionDefault, distritoSelect);
-
-    const idProvinciaSeleccionada = provinciaSelect.value;
-
-    console.log(idProvinciaSeleccionada);
-
-    const distritosSeleccionados = listaDistritos.filter(distrito => distrito["id_provincia"] === idProvinciaSeleccionada);
-
-    console.log(distritosSeleccionados);
-
-    for(const objeto of distritosSeleccionados) {
-        const nombreDistrito = objeto["distrito"];
-        const idDistrito = objeto["id_distrito"];
-        crearOptionSelect(nombreDistrito, idDistrito, distritoSelect);
-    }
-}
-
-const crearOptionSelect = (nombreZonaGeografica, idZonaGeografica, tipoSelect) => {
-    const nuevoOption = document.createElement('option');
-    nuevoOption.value = idZonaGeografica;
-    nuevoOption.innerHTML = nombreZonaGeografica;
-    tipoSelect.append(nuevoOption);
-}
-
-const crearOptionDefault = (contenidoOptionDefault, tipoSelect) => {
-    const nuevoOption = document.createElement('option');
-    nuevoOption.innerText = contenidoOptionDefault;
-    nuevoOption.id = 'opcion-disabled';
-    // nuevoOption.disabled = true;
-    tipoSelect.append(nuevoOption);
-}
-
-//Función para Deshabilitar la opción "-- Escoge un Departamento --"
-const deshabilitarOpcion = () => {
-    const opcionDisabled = document.getElementById('opcion-disabled');
-    opcionDisabled.disabled = true;
-}
-
-const listaDepartamentos = await axiosGet('https://geo-peru-api.onrender.com/department');
-const listaProvincias = await axiosGet('https://geo-peru-api.onrender.com/province');
-const listaDistritos = await axiosGet('https://geo-peru-api.onrender.com/district');
-
-//AGREGAR DEPARTAMENTOS AL SELECT
-mostrarDepartamentos();
-
-//AGREGAR PROVINCIAS AL SELECT
-
-//AGREGAR DISTRITOS AL SELECT
-
-
-console.log(listaDepartamentos);
-console.log(listaProvincias);
-console.log(listaDistritos);
-
-
-departamentoSelect.addEventListener('change', mostrarProvincias);
-provinciaSelect.addEventListener('change', mostrarDistritos);
-
-//Deshabilitar opción por defecto de los 3 Select
-// departamentoSelect.addEventListener('click', deshabilitarOpcion);
-// provinciaSelect.addEventListener('click', deshabilitarOpcion);
-// distritoSelect.addEventListener('click', deshabilitarOpcion);
+formulario.addEventListener('submit', funcion);
---
+++ front-end/js/geographicalArea.js
@@ -0,0 +1,97 @@
+const departamentoSelect = document.getElementById('departamentoSelect');
+const provinciaSelect = document.getElementById('provinciaSelect');
+const distritoSelect = document.getElementById('distritoSelect');
+
+const axiosGet = async (url) => {
+    try {
+        const response = await axios.get(url);
+        return response.data;
+    } catch(err) {
+        console.error('', err.message);
+    }
+}
+
+//Función sin Evento -> Mostrar Departamentos
+const mostrarDepartamentos = () => {
+    for(const objeto of listaDepartamentos) {
+        const nombreDepartamento = objeto["departamento"];
+        const idDepartamento = objeto["id_departamento"]
+        crearOptionSelect(nombreDepartamento, idDepartamento, departamentoSelect);
+    }
+}
+
+//Función con Evento 'change' -> Mostrar Provincias
+const mostrarProvincias = () => {
+    provinciaSelect.innerHTML = '';
+
+    const contenidoOptionDefault = '-- Escoge una Provincia --';
+    crearOptionDefault(contenidoOptionDefault, provinciaSelect);
+
+    const idDepartamentoSeleccionado = departamentoSelect.value;
+    console.log(idDepartamentoSeleccionado);
+
+    const provinciasSeleccionadas = listaProvincias.filter(provincia => provincia["id_departamento"] === idDepartamentoSeleccionado);
+    console.log(provinciasSeleccionadas);
+    
+    for(const objeto of provinciasSeleccionadas) {
+        const nombreProvincia = objeto["provincia"];
+        const idProvincia = objeto["id_provincia"];
+        crearOptionSelect(nombreProvincia, idProvincia, provinciaSelect);
+    }
+}
+
+//Función con Evento 'change' -> Mostrar Distritos
+const mostrarDistritos = () => {
+    distritoSelect.innerHTML = '';
+
+    const contenidoOptionDefault = '-- Escoge un Distrito --';
+    crearOptionDefault(contenidoOptionDefault, distritoSelect);
+
+    const idProvinciaSeleccionada = provinciaSelect.value;
+
+    console.log(idProvinciaSeleccionada);
+
+    const distritosSeleccionados = listaDistritos.filter(distrito => distrito["id_provincia"] === idProvinciaSeleccionada);
+
+    console.log(distritosSeleccionados);
+
+    for(const objeto of distritosSeleccionados) {
+        const nombreDistrito = objeto["distrito"];
+        const idDistrito = objeto["id_distrito"];
+        crearOptionSelect(nombreDistrito, idDistrito, distritoSelect);
+    }
+}
+
+const crearOptionSelect = (nombreZonaGeografica, idZonaGeografica, tipoSelect) => {
+    const nuevoOption = document.createElement('option');
+    nuevoOption.value = idZonaGeografica;
+    nuevoOption.innerHTML = nombreZonaGeografica;
+    tipoSelect.append(nuevoOption);
+}
+
+const crearOptionDefault = (contenidoOptionDefault, tipoSelect) => {
+    const nuevoOption = document.createElement('option');
+    nuevoOption.innerText = contenidoOptionDefault;
+    nuevoOption.id = 'opcion-disabled';
+    // nuevoOption.disabled = true;
+    tipoSelect.append(nuevoOption);
+}
+
+//Función para Deshabilitar la opción "-- Escoge un Departamento --"
+const deshabilitarOpcion = () => {
+    const opcionDisabled = document.getElementById('opcion-disabled');
+    opcionDisabled.disabled = true;
+}
+
+const listaDepartamentos = await axiosGet('https://geo-peru-api.onrender.com/department');
+const listaProvincias = await axiosGet('https://geo-peru-api.onrender.com/province');
+const listaDistritos = await axiosGet('https://geo-peru-api.onrender.com/district');
+
+//AGREGAR DEPARTAMENTOS AL SELECT
+mostrarDepartamentos();
+
+//AGREGAR PROVINCIAS AL SELECT
+departamentoSelect.addEventListener('change', mostrarProvincias);
+
+//AGREGAR DISTRITOS AL SELECT
+provinciaSelect.addEventListener('change', mostrarDistritos);
