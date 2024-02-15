0da3c44c80254ea66c8aef18dee0003607d1f708
JhuniorCq <114239158+JhuniorCq@users.noreply.github.com>
2024-02-14 21:50:53 -0500

    Inscribir escuela - listo

--- back-end/src/routes/routes.js
+++ back-end/src/routes/routes.js
@@ -14,25 +14,6 @@ router.put('/enrollSchool', updateSchool);
 // CONTROLLERS PROGRAMS
 router.get('/assignProgram', getSchools); //assignProgram -> Asignar Programa
 
-
-//RUTA PARA PROBAR LO DEL EXCEL
-// router.post('/probandoExcel', upload.single('excelStudents'), (req, res) => {
-//     try {//AL PONER upload.single ESTAMOS HACIENDO QUE SOLO SE PERMITA SUBIR UN SOLO ARCHIVO
-//         //Multer lo que hace es que -> Cuando pasamos a través del Middleware upload.single('excelStudents') el excelStudents que le pasemos lo va a colocar toda su información dentro de -> req.file
-//         console.log(req.file);
-//         saveExcel(req.file);
-//         res.send('Termina');
-//     } catch(err) {
-//         console.log('Error al descomponer el Excel', err.message);
-//     }
-// });
-
-// function saveExcel(file) { //Con esta función vamos a 
-//     const newPath = `./uploads/${file.originalname}`;
-//     fs.renameSync(file.path, newPath);
-//     return newPath;
-// }
-
 module.exports = {
     router
 };
