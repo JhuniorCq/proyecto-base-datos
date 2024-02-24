43729ccffc12b1ec9e4c009075d7ac2d199b3c6c
JhuniorCq <114239158+JhuniorCq@users.noreply.github.com>
2024-02-23 22:49:33 -0500

    .

--- front-end/js/modifySchool.js
+++ front-end/js/modifySchool.js
@@ -14,22 +14,49 @@ import {
 
 import {listaDepartamentos, listaProvincias, listaDistritos} from './geographicalArea.js';
 
-const modificarEscuela = (evento) => {
-    evento.preventDefault();
-    
-    const codigoModularValor = nuevoCodigoModular.value;
-    const nombreEscuela = nuevoNombreEscuela.value;
-    const direccionEscuela = nuevaDireccion.value;
-    const departamentoEscuela = obtenerNombreZonaGeografica(departamentoSelect.value, listaDepartamentos, 'id_departamento', 'departamento');
-    const provinciaEscuela = obtenerNombreZonaGeografica(provinciaSelect.value, listaProvincias, 'id_provincia', 'provincia');
-    const distritoEscuela = obtenerNombreZonaGeografica(distritoSelect.value, listaDistritos, 'id_distrito', 'distrito');
-    const nombresDirector = nuevosNombresDirector.value;
-    // const
+const modificarEscuela = async (evento) => {
+    try {
+        evento.preventDefault();
+        
+        const codigoModularValor = nuevoCodigoModular.value;
+        const nombreEscuela = nuevoNombreEscuela.value;
+        const direccionEscuela = nuevaDireccion.value;
+        const departamentoEscuela = obtenerNombreZonaGeografica(departamentoSelect.value, listaDepartamentos, 'id_departamento', 'departamento');
+        const provinciaEscuela = obtenerNombreZonaGeografica(provinciaSelect.value, listaProvincias, 'id_provincia', 'provincia');
+        const distritoEscuela = obtenerNombreZonaGeografica(distritoSelect.value, listaDistritos, 'id_distrito', 'distrito');
+        const nombresDirector = nuevosNombresDirector.value;
+        const apellidosDirector = nuevosApellidosDirector.value;
+        const celularDirector = nuevoCelularDirector.value;
+        const correoDirector = nuevoCorreoDirector.value;
+        // const excelEstudiantes = nuevoExcelEstudiantes.value;
 
-    //SI NO SE AGREGA NADA EN LOS INPUTS SE RECOGE UNA CADENA VACÍA
-    console.log(nuevoExcelEstudiantes.value === '');
+        const formData = new FormData();
+        // codigoModularValor === '' ? true: false;
+        
+        formData.append('modular_code', codigoModularValor);
+        formData.append('name_school', nombreEscuela);
+        formData.append('address', direccionEscuela);
+        formData.append('district_name', departamentoEscuela);
+        formData.append('province_name', provinciaEscuela);
+        formData.append('department_name', distritoEscuela);
+        formData.append('director_name', nombresDirector);
+        formData.append('director_lastname', apellidosDirector);
+        formData.append('director_cellphone', celularDirector);
+        formData.append('director_email', correoDirector);
+        formData.append('excelStudents', nuevoExcelEstudiantes.files[0]);
 
-    
+        const url = 'http://localhost:3000/modifySchool';
+            
+        
+        const response = await axios.put(url, formData);
+        const respuesta = response.data;
+
+        alert(respuesta);
+
+    } catch(err) {
+        console.error('Error en modificarEscuela en modifySchool.js', err.message);
+        alert('Error al modificar los datos.');
+    }
 }
 
 const obtenerNombreZonaGeografica = (zonaGeograficaValue, listaZonaGeografica, clave_id, clave_nombre) => {
